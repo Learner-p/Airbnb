3609246a582f035a6334853483ea9481f0223a01
Kaivalya Mane <kaivalyamane129@gmail.com>
2025-06-28 12:40:22 +0530

    Update listing.js
    Search bar commit

--- routes/listing.js
+++ routes/listing.js
@@ -3,53 +3,49 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner , validateListing} = require("../middleware.js");
-const  listingController = require("../controllers/listing.js");
+const listingController = require("../controllers/listing.js");
 const multer = require("multer");
 const {storage} = require("../cloudConfig.js")
 const upload = multer({storage});
 
-router
-    .route("/")
-    .get("/", wrapAsync(async (req, res) => {
-        const { q, country } = req.query;
-        let filter = {};
+// Correct GET and POST routes for "/"
+router.get("/", wrapAsync(async (req, res) => {
+    const { q, country } = req.query;
+    let filter = {};
 
-        if (q) {
-            filter.$or = [
-                { title: { $regex: q, $options: "i" } },
-                { location: { $regex: q, $options: "i" } },
-                { country: { $regex: q, $options: "i" } }
-            ];
-        }
-        if (country) {
-            filter.country = country;
-        }
+    if (q) {
+        filter.$or = [
+            { title: { $regex: q, $options: "i" } },
+            { location: { $regex: q, $options: "i" } },
+            { country: { $regex: q, $options: "i" } }
+        ];
+    }
+    if (country) {
+        filter.country = country;
+    }
 
-        const allListings = await Listing.find(filter);
-        res.render("listings/index", { allListings, q, country });
-    }))
-    .post(
-        isLoggedIn,
-        upload.single('listing[image]'),
-        validateListing,
-        wrapAsync(listingController.CreateListing)
-    );
+    const allListings = await Listing.find(filter);
+    res.render("listings/index", { allListings, q, country });
+}));
 
+router.post("/",
+    isLoggedIn,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.CreateListing)
+);
 
-//New Route
-router.get("/new",isLoggedIn , listingController.renderNewform);
+// New Route
+router.get("/new", isLoggedIn, listingController.renderNewform);
 
-
+// Show, Update, Delete routes
 router
     .route("/:id")
-    .get( wrapAsync(listingController.showListing))   //Show route
-    .put( isLoggedIn , isOwner, upload.single('listing[image]'), wrapAsync(listingController.updateListing))   //update route
-    .delete( isLoggedIn ,isOwner, wrapAsync(listingController.destroyListing));  //Delete route
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, upload.single('listing[image]'), wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
+// Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-//Edit Route
-router.get("/:id/edit", isLoggedIn,isOwner, wrapAsync( listingController.renderEditForm));
-
-
-
-module.exports = router ;
+module.exports = router;
